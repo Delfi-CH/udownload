f28d787984c40618913c89837e2932a4f318e149
Delfi-CH <laspitzer@gmx.ch>
2026-02-13 17:34:40 +0100

    add docs + remake tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 //! Wrapper around the crate 'ureq' to make downloading files easier
 //! 
-//!
-//!
+//!  Example:
+//! ```
+//! use udownload::download;
+//! use std::path::PathBuf;
 //! 
-//! 
+//! let downloaded_file_path: PathBuf = download("https://pokeapi.co/api/v2/pokemon/golbat", PathBuf::from("file.json")).unwrap();
+//! ```
 
 use std::{fs::File, path::PathBuf, io::{Read, Write}};
 
@@ -22,7 +25,7 @@ pub enum Error {
     Misc(String),
 }
 
-
+/// Download a file with parameters `url: &str` `save_path: PathBuf` and returns a `Result<PathBuf, Error>`
 pub fn download(url: &str, save_path: PathBuf) -> Result<PathBuf, Error> {
 
     let mut response = ureq::get(url).call()?;
@@ -43,6 +46,7 @@ pub fn download(url: &str, save_path: PathBuf) -> Result<PathBuf, Error> {
 }
 
 #[cfg(feature = "progress-bar")]
+/// Same function as before , but with a progess bar
 pub fn download_with_progressbar(url: &str, save_path: PathBuf) -> Result<PathBuf, Error> {
     let mut response = ureq::get(url).call()?;
     let size = response.body().content_length().unwrap_or(0);
@@ -81,11 +85,22 @@ mod tests {
 
     #[test]
     fn test_download() {
+        let mut server = mockito::Server::new();
+
+        let url = server.url();
+
+        server.mock("GET", "/hello")
+            .with_status(201)
+            .with_header("content-type", "text/plain")
+            .with_header("x-api-key", "1234")
+            .with_body("world")
+            .create();
+
         let tmpdir = temp_dir();
-        let downloaded_file_path: PathBuf = download("https://pokeapi.co/api/v2/pokemon/golbat", tmpdir.join("file.json")).unwrap_or(PathBuf::new());
+        let downloaded_file_path: PathBuf = download(&(url + "/hello"), tmpdir.join("file.txt")).unwrap();
         let downloaded_file_contents = read_to_string(downloaded_file_path).unwrap_or("download".to_string());
 
-        let local_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("example.json");
+        let local_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("example.txt");
         let local_file_contents = read_to_string(local_file_path).unwrap_or("local".to_string());
 
         assert_eq!(downloaded_file_contents, local_file_contents);
